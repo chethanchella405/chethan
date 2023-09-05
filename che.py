fa1bbf8695bb1d19685ef410f5458cc415c44dcf
chethanchella405 <143305171+chethanchella405@users.noreply.github.com>
2023-09-05 11:16:30 +0530

    Update che.py

--- che.py
+++ che.py
@@ -1,5 +1,5 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num1 = float(input("Enter the first number: 12 "))
+num2 = float(input("Enter the second number: 32 "))
 
 # Add the numbers
 result = num1 + num2
