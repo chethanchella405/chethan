ac9b415507aa51eed25e037ef303b67548045e9a
chethanchella405 <143305171+chethanchella405@users.noreply.github.com>
2023-09-05 11:17:32 +0530

    Update che.py

--- che.py
+++ che.py
@@ -1,8 +1,4 @@
-num1 = float(input("Enter the first number: 12 "))
-num2 = float(input("Enter the second number: 32 "))
-
-# Add the numbers
+num1 = 12
+num2 = 21
 result = num1 + num2
-
-# Display the result
 print(f"The sum of {num1} and {num2} is {result}")
