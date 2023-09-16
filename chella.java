1c6b9ee94ef9c09656055821e7d7ad497eeb875f
chethanchella405 <143305171+chethanchella405@users.noreply.github.com>
2023-09-16 13:23:04 +0530

    Update chella.java

--- chella.java
+++ chella.java
@@ -4,10 +4,10 @@ class MyClass {
   public static void main(String[] args) {
     int x, y, sum;
     Scanner myObj = new Scanner(System.in); // Create a Scanner object
-    System.out.println("Type a number:");
+    System.out.println("5");
     x = myObj.nextInt(); // Read user input
 
-    System.out.println("Type another number:");
+    System.out.println("5");
     y = myObj.nextInt(); // Read user input
 
     sum = x + y;  // Calculate the sum of x + y
